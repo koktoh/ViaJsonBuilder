ad5d739e2572d8e00e435b57e23b6d3ec06c5ea8
koktoh <koktoh96@gmail.com>
2020-07-28 17:01:10 +0900

    add QMK Configurator json model

---
+++ ViaJsonBuilder/Models/Json/QmkConfigurator/QcLayout.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace ViaJsonBuilder.Models.Json.QmkConfigurator
+{
+    public class QcLayout
+    {
+        [DataMember(Name = "layout")]
+        public IEnumerable<QcKey> QcKeys { get; set; }
+    }
+}
