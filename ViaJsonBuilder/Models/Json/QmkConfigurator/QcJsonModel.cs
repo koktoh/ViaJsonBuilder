3baf8bfb356542f6357f5608a0e51490f39cd3f4
koktoh <koktoh96@gmail.com>
2020-07-30 20:56:19 +0900

    fix type of properties in QMK Configurator's json model

--- ViaJsonBuilder/Models/Json/QmkConfigurator/QcJsonModel.cs
+++ ViaJsonBuilder/Models/Json/QmkConfigurator/QcJsonModel.cs
@@ -12,9 +12,9 @@ namespace ViaJsonBuilder.Models.Json.QmkConfigurator
         [DataMember(Name = "maintainer")]
         public string Maintainer { get; set; }
         [DataMember(Name = "width")]
-        public int Width { get; set; }
+        public double Width { get; set; }
         [DataMember(Name = "height")]
-        public int Height { get; set; }
+        public double Height { get; set; }
         [DataMember(Name = "layouts")]
         public IDictionary<string, QcLayout> QcLayouts { get; set; }
     }
